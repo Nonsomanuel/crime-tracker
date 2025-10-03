df1a4f5de1c528781be09cd8955fcbdd1ae1399f
Nonsomanuel <immannuell234@gmail.com>
2025-10-03 22:11:50 +0100

    report-form-update

--- src/modules/reportcrime/crimeform/index.tsx
+++ src/modules/reportcrime/crimeform/index.tsx
@@ -318,7 +318,7 @@ export function ReportForm() {
             )}
           </div>
           <Button className="text-[14px] px-4 py-2.5" type="submit">
-            Submit
+            Submit Report
           </Button>
         </form>
       </Form>
